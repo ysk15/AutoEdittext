6dc5bd048e4cbcc9366ae5e6e32bdf4f314b232d
ysk15 <ysakh15@gmail.com>
2017-07-28 15:06:35 +0530

    Upload

---
+++ app/src/main/java/com/example/ociuz/customlibraryexample/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.ociuz.customlibraryexample;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.autotextinput.CreateInputs;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String [] names = {"name","age:number","place","mob:number","email","username","pass:password"};
+        final RelativeLayout rl = (RelativeLayout)findViewById(R.id.activity_main);
+        new CreateInputs(MainActivity.this,names,rl).create();
+        final Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> nm = new ArrayList<String>(new CreateInputs(MainActivity.this,names,rl).getvalues(rl)) ;
+                if(nm.size()==names.length){
+                    String name = nm.get(0);
+                    String age = nm.get(1);
+                    String place = nm.get(2);
+                    String mob = nm.get(3);
+                    String email = nm.get(4);
+                    String username = nm.get(5);
+                    String pass = nm.get(6);
+                    b.setText(name+"\n"+age+"\n"+place+"\n"+mob+"\n"+email+"\n"+username+"\n"+pass+"\n");
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public  static void create(Context cx, String [] names, RelativeLayout rl){
+        LinearLayout li = new LinearLayout(cx);
+        li.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        li.setOrientation(LinearLayout.VERTICAL);
+        int sdk = android.os.Build.VERSION.SDK_INT;
+        int jellyBean = android.os.Build.VERSION_CODES.JELLY_BEAN;
+        for(int i=0;i<names.length;i++){
+            EditText ed = new EditText(cx);
+            ed.setHint(names[i]);
+            ed.setId(i);
+            ed.setBackgroundResource(R.drawable.shape);
+            ed.setPadding(5,5,5,5);
+            ed.setHintTextColor(Color.BLACK);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(30,15,30,15);
+            ed.setLayoutParams(params);
+            li.addView(ed);
+
+        }
+        rl.addView(li);
+
+
+    }
+}
---
+++ autotextinput/src/main/java/com/example/autotextinput/CreateInputs.java
@@ -0,0 +1,75 @@
+package com.example.autotextinput;
+
+import android.content.Context;
+import android.text.InputType;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ociuz on 7/28/2017.
+ */
+
+public class CreateInputs {
+    private    Context cx;
+    private String[] names;
+    private   RelativeLayout rl;
+
+
+    public CreateInputs(Context cx, String[] names, RelativeLayout rl) {
+        this.cx = cx;
+        this.names = names;
+        this.rl = rl;
+    }
+
+    public void create(){
+        LinearLayout li = new LinearLayout(cx);
+        li.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        li.setOrientation(LinearLayout.VERTICAL);
+        for(int i=0;i<names.length;i++){
+            EditText ed = new EditText(cx);
+            ed.setHint(names[i]);
+            ed.setId(i);
+            ed.setBackgroundResource(R.drawable.shape);
+            ed.setPadding(5,5,5,5);
+            ed.setGravity(View.TEXT_ALIGNMENT_CENTER);
+            ed.setMaxLines(1);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(30,20,30,20);
+            ed.setLayoutParams(params);
+            if(names[i].contains(":")){
+                if(names[i].split(":")[1].equals("number")){
+                    ed.setInputType(InputType.TYPE_CLASS_PHONE );
+                    ed.setHint(names[i].split(":")[0]);
+
+                }
+                else if(names[i].split(":")[1].equals("password")){
+                    ed.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    ed.setHint(names[i].split(":")[0]);
+                }
+            }
+            li.addView(ed);
+        }
+        rl.addView(li);
+    }
+    public  ArrayList<String> getvalues(RelativeLayout rl){
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i=0;i<names.length;i++){
+            EditText ed = (EditText)rl.findViewById(i);
+            if(!ed.getText().toString().equals("")){
+                arr.add(ed.getText().toString());
+            }
+        }
+        return arr;
+
+    }
+
+
+
+
+}
